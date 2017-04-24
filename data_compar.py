74583801b35af08cf3564db1a5e0fcbf0d63e137
omri golan <gomri@github.com>
2017-04-24 14:27:22 +0300

    commented out 24 hour serving tests

--- data_compar.py
+++ data_compar.py
@@ -9,7 +9,7 @@ def check_24_hours_serving(alist):
         test_result = False
         return test_result
 
-serving_24_hours_test_result = check_24_hours_serving(result_24_hour_serving)
-
-print test_result_formater("\n24 Hours exist: ",serving_24_hours_test_result)
+# serving_24_hours_test_result = check_24_hours_serving(result_24_hour_serving)
+#
+# print test_result_formater("\n24 Hours exist: ",serving_24_hours_test_result)
 
