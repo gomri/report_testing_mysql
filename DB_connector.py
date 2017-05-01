83a865f5e83dbd4daaf28edeaed4775109da2c97
omri golan <gomri@github.com>
2017-05-01 19:07:43 +0300

    refactoring - finished compare daily to hourly rev, imps test. - formatted test results. - execute tests from command line and choose what test to run

--- DB_connector.py
+++ DB_connector.py
@@ -11,9 +11,13 @@ FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 
 serving_24_expect_result = 24
 
-key_serving_24_hours = "hours_serving"
+serving_24_massage = "24 hours serving: "
+compare_daily_hourly_massage = "Compare daily to hourly reports: "
 
-key_daily_hourly = ['IMPs','Rev']
+
+# TODO: think what to do about the fact i need Rev, IMPs from 2 queries maybe write a global ver (ask dror)
+
+percent_threshold = 0.01  # TODO: ask michal what she wants the threshold to be
 
 DB_connection_config = {
   'user': 'omrig',
@@ -36,8 +40,8 @@ logging.basicConfig(format=FORMAT,level=logging.DEBUG,filename=LOGGER_FILE)
 
 def user_input():
     parser = argparse.ArgumentParser(description='Choose a test to run')
-    parser.add_argument("serving",help='Run 24 hours serving test')
-    parser.add_argument("daily_hourly",help="Run compare daily to hourly data test")
+    parser.add_argument("test_num",type=int,help='1. 24 hours serving'
+                                                        '\n2. Compare daily hourly rev, IMPs')
     return parser.parse_args()
 
 
@@ -72,43 +76,75 @@ def fetch_result(cursor):
     result = cursor.fetchone()
     return result
 
-def checking_results(expected, result):
-    if expected == result:
-        return True
 
-def round_mum(num, num_after_zero):
-    rounded_num = round(num, num_after_zero)
-    return rounded_num
+def diff_in_percent(num1,num2):
+    percent_diff = 100 * (num2-num1) / num1
+    return percent_diff
+
+
+def compare_results(expected, result):
+    if expected == result:
+            return True
+    else:
+        return False
+
+
+def compare_percent(threshold, result):
+    if threshold > result:
+        return True
+    else:
+        return False
+
+
+# def round_mum(num, num_after_zero):
+#     rounded_num = round(num, num_after_zero)
+#     return rounded_num
 
 # Creates connection to DB
 cnx = create_connection(**DB_connection_config)
 
 
 # Gets test to run from user via command line
-# test_to_run = user_input()
+test_to_run = user_input()
 
-# if test_to_run.serving:
-#     cursor = connect_to_DB(cnx)
-#     serving_24_cursor = execute_query(cursor=cursor, query=query_serving_24_hours)
-#
-#     result_24_serving = fetch_result(cursor=serving_24_cursor,dict_keys=key_serving_24_hours)
-#     print checking_results(expected=serving_24_expect_result, result=result_24_serving)
-# TODO: Format test result so they are more indicative
+if test_to_run.test_num == 1:
+    cursor = connect_to_DB(cnx)
+    serving_24_cursor = execute_query(cursor=cursor, query=query_serving_24_hours)
 
-# elif test_to_run.daily_hourly:
-cursor_hourly = connect_to_DB(cnx)
-cursor_daily = connect_to_DB(cnx)
+    result_24_serving = fetch_result(cursor=serving_24_cursor)
+    serving_hours = result_24_serving.get("hours_serving")
 
-hourly_cursor = execute_query(cursor=cursor_hourly, query=query_hourly_daily_match_hourly)
-daily_cursor = execute_query(cursor=cursor_daily, query=query_hourly_daily_match_daily)
+    serving_24_cursor.close()
 
-result_hourly = fetch_result(cursor=hourly_cursor)
-Rev_hourly = round(result_hourly.get("Rev"),2)
-IMPs_hourly = round(result_hourly.get("IMPs"),2)
+    if compare_results(serving_24_expect_result,serving_hours):
+        print serving_24_massage, True
+    else:
+        print serving_24_massage, False
 
-result_daily = fetch_result(cursor=daily_cursor)
-Rev_daily = round(result_daily.get("Rev"),2)
-IMPs_daily = round(result_daily.get("IMPs"),2)
+elif test_to_run.test_num  == 2:
+    cursor_hourly = connect_to_DB(cnx)
+    cursor_daily = connect_to_DB(cnx)
 
-cursor_hourly.close()
-cursor_daily.close()
+    hourly_cursor = execute_query(cursor=cursor_hourly, query=query_hourly_daily_match_hourly)
+    daily_cursor = execute_query(cursor=cursor_daily, query=query_hourly_daily_match_daily)
+
+    result_hourly = fetch_result(cursor=hourly_cursor)
+    rev_hourly = round(result_hourly.get("Rev"),2)
+    imps_hourly = round(result_hourly.get("IMPs"),2)
+
+    result_daily = fetch_result(cursor=daily_cursor)
+    rev_daily = round(result_daily.get("Rev"),2)
+    imps_daily = round(result_daily.get("IMPs"),2)
+
+    percent_diff_rev = diff_in_percent(rev_hourly,rev_daily)
+    percent_diff_imps = diff_in_percent(imps_hourly,imps_daily)
+
+    hourly_cursor.close()
+    daily_cursor.close()
+
+    if compare_percent(percent_threshold, percent_diff_rev) and compare_percent(percent_threshold, percent_diff_imps):
+        print compare_daily_hourly_massage,True
+    else:
+        print compare_daily_hourly_massage,False
+
+
